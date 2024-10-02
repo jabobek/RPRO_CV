dd00b8e9ce5127722186aca46d138feebf3f919f
Bobek Jaromír <bobekja1@uhk.cz>
2024-10-02 11:29:42 +0200

    Initial commit

---
+++ src/test/java/org/example/rpro_cv/RproCvApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.rpro_cv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RproCvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
